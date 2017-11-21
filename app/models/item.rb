1515e1e88519008ed7ec45c4324257120f3d7d1e
deeteecg <dt.scott001@gmail.com>
2017-11-21 16:39:05 +0800

    search function done

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
-  
   belongs_to :user
+
+
+  def self.search(search)
+    # search = search.downcase
+    self.where("item_name ILIKE ? OR
+                brand ILIKE ?",
+                "%#{search}%",
+                "%#{search}%")
+  end
+
+
 end
